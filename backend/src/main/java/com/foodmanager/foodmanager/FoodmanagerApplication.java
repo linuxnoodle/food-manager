4ffe713b8257bd33d82ee03ae7041ff737377cd2
qed <ipadnoodle@gmail.com>
2026-05-28 14:59:53 -0400

    initial commit

---
+++ backend/src/main/java/com/foodmanager/foodmanager/FoodmanagerApplication.java
@@ -0,0 +1,13 @@
+package com.foodmanager.foodmanager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FoodmanagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FoodmanagerApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/foodmanager/foodmanager/FoodmanagerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.foodmanager.foodmanager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FoodmanagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
