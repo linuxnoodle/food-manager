072e74f896e4f9811f4895cd2a2983aaa0759e98
Caleb Singleton <cmsingle12@gmail.com>
2026-06-24 09:54:45 -0400

    updated central app file

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,15 +1,19 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import Login from './pages/Login'
+import Register from './pages/Register'
 import Dashboard from './pages/Dashboard'
 import FoodSearch from './pages/FoodSearch'
+import RecipeCreation from './pages/RecipeCreation'
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Login />} />
+        <Route path="/register" element={<Register />} />
         <Route path="/dashboard" element={<Dashboard />} />
         <Route path="/food-search" element={<FoodSearch />} />
+        <Route path="/recipe-creation" element={<RecipeCreation />} />
       </Routes>
     </BrowserRouter>
   )
